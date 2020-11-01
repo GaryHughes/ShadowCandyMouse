479ed8e995fc95ef80f3f1be688000d09dde48dd
Gary Hughes <garyedwardhughes@gmail.com>
2020-11-01 13:30:43 +1100

    Initial Commit

---
+++ ShadowCandyMouse/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ShadowCandyMouse
+//
+//  Created by Gary Hughes on 25/7/20.
+//  Copyright © 2020 Gary Hughes. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
